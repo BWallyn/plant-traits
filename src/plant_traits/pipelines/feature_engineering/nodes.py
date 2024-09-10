48a340147775898f7ac1af82a002e2130da719a8
BWallyn <ben96.wallyn@gmail.com>
2024-09-10 22:40:03 +0200

    function to add path to images

--- src/plant_traits/pipelines/feature_engineering/nodes.py
+++ src/plant_traits/pipelines/feature_engineering/nodes.py
@@ -2,3 +2,23 @@
 This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 0.19.8
 """
+# =================
+# ==== IMPORTS ====
+# =================
+import pandas as pd
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+def add_path_to_images(df: pd.DataFrame, path_images: str) -> pd.DataFrame:
+    """Add path to each image in the dataframe
+
+    Args:
+        df (pd.DataFrame): Input dataframe
+        path_images (str): Path to the folder containing images
+    Returns:
+        df (pd.DataFrame): Output dataframe with path to images
+    """
+    df['file_path'] = df['id'].apply(lambda s: f'{path_images}/train_images/{s}.jpeg')
+    return df
