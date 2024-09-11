a2563aafe1b7849457f54d94a3802a26aac9ba53
BWallyn <ben96.wallyn@gmail.com>
2024-09-11 07:45:03 +0200

    split dataset into train and test

--- src/plant_traits/pipelines/feature_engineering/nodes.py
+++ src/plant_traits/pipelines/feature_engineering/nodes.py
@@ -5,7 +5,9 @@ generated using Kedro 0.19.8
 # =================
 # ==== IMPORTS ====
 # =================
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # ===================
 # ==== FUNCTIONS ====
@@ -22,3 +24,16 @@ def add_path_to_images(df: pd.DataFrame, path_images: str) -> pd.DataFrame:
     """
     df['file_path'] = df['id'].apply(lambda s: f'{path_images}/train_images/{s}.jpeg')
     return df
+
+
+def split_train_test(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Split the dataset into train and test
+
+    Args:
+        df (pd.DataFrame): Input dataframe
+    Returns:
+        df_train (pd.DataFrame): Training dataframe
+        df_test (pd.DataFrame): Test dataframe
+    """
+    df_train, df_test = train_test_split(df, test_size=0.2, shuffle=True, random_state=12)
+    return df_train, df_test
