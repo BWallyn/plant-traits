6b41c2137a9cabb59699790574d6ee53a05484fe
BWallyn <ben96.wallyn@gmail.com>
2024-09-11 08:24:56 +0200

    pipeline to prepare data

--- src/plant_traits/pipelines/feature_engineering/pipeline.py
+++ src/plant_traits/pipelines/feature_engineering/pipeline.py
@@ -3,8 +3,31 @@ This is a boilerplate pipeline 'feature_engineering'
 generated using Kedro 0.19.8
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, node, pipeline
+
+from plant_traits.pipelines.feature_engineering.nodes import (
+    add_path_to_images,
+    split_train_test,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        pipe=[
+            node(
+                func=add_path_to_images,
+                inputs=["df_raw", "params:path_images"],
+                outputs="df_w_path",
+                name="add_path_to_images"
+            ),
+            node(
+                func=split_train_test,
+                inputs="df_w_path",
+                outputs=["df_train", "df_valid"],
+                name="split_data_train_validation"
+            ),
+        ],
+        inputs="df_raw",
+        outputs=["df_train", "df_valid"],
+        namespace="prepare_data"
+    )
